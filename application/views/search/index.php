80ec74b63f957d1f581ccc0838b90f795c22e335
Steven Dumbura <stevedumbura@gmail.com>
2015-07-07 22:32:17 +0200

    added home interface and find & search controllers

---
+++ application/views/search/index.php
@@ -0,0 +1,35 @@
+
+<div class="ui grid">
+  <div class="four wide column"></div>
+  <div class="eight wide column">
+    <div class="ui raised segment">
+    <div class="ui divider"></div>
+      <div class="ui ordered steps">
+        <div class="completed step">
+          <div class="content">
+            <div class="title">Personal Info</div>
+          </div>
+        </div>
+        <div class="disabled step">
+          <div class="content">
+            <div class="title">Symptom Search</div>
+          </div>
+        </div>
+        <div class="disabled step">
+          <div class="content">
+            <div class="title">Get Assistance</div>
+          </div>
+        </div>
+          <div class="disabled step">
+          <div class="content">
+            <div class="title">Book Appointment</div>
+          </div>
+        </div>
+      </div>
+      <div class="ui divider"></div>
+</div>
+  </div>
+  <div class="four wide column"></div>
+</div>
+
+
